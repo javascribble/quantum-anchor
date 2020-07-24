fad8e559ed70fe057d4b57acc0916c45fc71aec9
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-24 09:08:18 -0400

    update reference

--- source/elements/element.js
+++ source/elements/element.js
@@ -1,14 +1,11 @@
-import { Component, template } from '../../references/quantum.js';
 import html from '../templates/anchor.js';
 
-export class Anchor extends Component {
+export class Anchor extends quantum.Component {
     constructor() {
         super();
     }
 
-    static template = template(html);
-
-    static attributes = [];
+    static template = quantum.template(html);
 }
 
-customElements.define('quantum-anchor', Anchor);
+quantum.define('quantum-anchor', Anchor);
