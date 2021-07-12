7a502874bb1f0cd105c05162d4c60ae56454d0fb
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:24:53 -0400

    update build

--- source/elements/anchor.js
+++ source/elements/anchor.js
@@ -1,5 +1,9 @@
+import anchor from '../templates/anchor.js';
+
 export class Anchor extends Quantum {
     constructor() {
         super();
     }
 }
+
+Anchor.define('quantum-anchor', anchor);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Anchor } from './elements/anchor.js';
-import anchor from './templates/anchor.js';
-
-Anchor.define('quantum-anchor', anchor);
